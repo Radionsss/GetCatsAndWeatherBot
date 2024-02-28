9574dc5b06bf37f823313685194e5e4559c94180
Radname <radion20077@gmail.com>
2024-02-28 11:13:47 +0600

    Добавлены новые функции

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
-  telegramToken: "6406173524:AAFVukwO8mX5Falbuk3alWCh2g_I5gfigF4",
-  catUrl: "https://api.thecatapi.com/v1/images/search",
+  telegramToken: "",
+  catUrl: "",
   weatherUrl:
-    "http://api.weatherapi.com/v1/current.json?key=766bf56e922e44fe90364604242702&q=",
+    "",
 };
